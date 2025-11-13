1fa802521ea70b58675992c7805e2cce50c908a0
RahulCSS <rahulcs1618@gmail.com>
2025-11-13 18:01:35 +0530

    Basic structure & components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='container'>
+
+      {/* Header section */}
+      <div className='header-section'>
+        <h1>Weather Now</h1>
+      </div>
+
+      {/* Hero section */}
+      <div className='hero-section'>
+        <h1>How's the sky looking today?</h1>
+      </div>
+
+      {/* Weather data section */}
+      <div className='weather-Data-section'>
+
+        {/* Left column section */}
+        <div className='left-column-section'>
+
+        </div>
+
+        {/* Right column section */}
+        <div className='right-column-section'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/DetailedWeather.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailedWeather = () => {
+  return (
+    <div>DetailedWeather</div>
+  )
+}
+
+export default DetailedWeather
